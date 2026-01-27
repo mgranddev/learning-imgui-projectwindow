0e5fd4cf5f401db1dfa1a6c1362bc9e273bcc91d
Michael Grand <michael@mgrand.dev>
2026-01-27 10:36:29 -0800

    Register to projectWindowItemOnGUI
    - Register to `projectWindowItemOnGUI` event when show badges is
        enabled, and unregister when disabled.
    - Correctly handles domain reloads.
    - Adds an event counter and a menu option to display the current count.

--- Assets/ProjectWindow/P01_CornerBadge/Editor/ProjectWindowBadges.cs
+++ Assets/ProjectWindow/P01_CornerBadge/Editor/ProjectWindowBadges.cs
@@ -1,10 +1,20 @@
 using UnityEditor;
+using UnityEngine;
 
 namespace MGrand.ProjectWindow.P01_CornerBadge.Editor
 {
+    [InitializeOnLoad]
     public static class ProjectWindowBadges
     {
         private const string ShowBadgesMenu = "Tools/Project Window Badges/Show Badges";
+
+        static ProjectWindowBadges()
+        {
+            if (CornerBadgePreferences.instance.ShowBadges)
+            {
+                EditorApplication.projectWindowItemOnGUI += OnProjectWindowItemGUI;
+            }
+        }
 
         [MenuItem(ShowBadgesMenu, true)]
         public static bool ValidateShowBadges()
@@ -17,6 +27,26 @@ namespace MGrand.ProjectWindow.P01_CornerBadge.Editor
         public static void ToggleShowBadges()
         {
             CornerBadgePreferences.instance.ToggleShowBadges();
+            if (CornerBadgePreferences.instance.ShowBadges)
+            {
+                EditorApplication.projectWindowItemOnGUI += OnProjectWindowItemGUI;
+            }
+            else
+            {
+                EditorApplication.projectWindowItemOnGUI -= OnProjectWindowItemGUI;
+                PrintEventCount();
+            }
+        }
+
+        [MenuItem("Tools/Project Window Badges/Print Event Count")]
+        public static void PrintEventCount()
+        {
+            Debug.Log($"Number of events received: {CornerBadgePreferences.instance.NumEventsReceived}");
+        }
+
+        private static void OnProjectWindowItemGUI(string guid, Rect selectionRect)
+        {
+            CornerBadgePreferences.instance.ReceivedEvent();
         }
     }
 }
